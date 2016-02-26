83f44bdf6189c9a836c559a2764f1aced61001b3
Daniel Tabion <dctabion@gmail.com>
2016-02-26 14:19:23 -0600

    fix resume download

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Daniel Tabion' });
 });
 
+router.get('/resume', function(req, res) {
+  var file = 'public/assets/ResumeDCTabion.pdf';
+  res.download(file);
+});
+
 module.exports = router;
